7aeb426b8dcdaa9c5e0709f93bebafd8bbd26383
Vibhor Joshi <vibhorjoshi05@gmail.com>
2018-05-24 12:58:30 +0530

    Update TripletSolve.java

--- RESET/DS/TripletSolve.java
+++ RESET/DS/TripletSolve.java
@@ -1,4 +1,7 @@
 public class TripletSolve{
+    
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
     static int[] solve(int[] a, int[] b) {
         System.out.println("----------------FIRST LINE OF SOLVE---------------------");
         
@@ -9,8 +12,21 @@ public class TripletSolve{
 
     }
 
-    public static void main(String[] args) {
+    public static int[] readArray()throws IOException{
+        int[] arr;
+        String[] srr = br.readLine().trim().split(" ");
+        arr =  new int[srr.length];
+        for(int i = 0; i<srr.length;i++){
+            arr[i]=Integer.parseInt(srr[i]);
+        }
+        return arr;
+    }
+
+    public static void main(String[] args)throws IOException {
         System.out.println("-----------FIRST LINE OF MAIN------------");
-        
+        int[] alice = readArray();
+        int[] bob = readArray();
+        int[] result = solve(alice,bob);
+        System.out.printf("Alice :%d Bob:%d",result[0],result[1]);
     }
 }
